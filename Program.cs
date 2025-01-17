9360016aac327d86b58f2b237849e8acef473b86
Manya727 <buzamartin727@gmail.com>
2025-01-17 12:36:57 +0100

    Osszeadas_kesz

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        static int Osszeadas(int a, int b){
+            return a+b;
+        }
+    }
+}
